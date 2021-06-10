f41238b4072c862546b0ec07e6fdfdfa523ab2ca
thunderblog <hiroki3373@gmail.com>
2021-06-10 20:04:34 +0900

    不格好ですが、とりあえず実装完了

--- js/script.js
+++ js/script.js
@@ -3,5 +3,7 @@
 $('.humburger-button').on('click',function(){
 	$('.humburger-button').toggleClass('close');
 	$('.nav-wrapper').fadeToggle('slide-in');
+	$('.nav-list').toggleClass('humburger-on');
+	$('.header__menu').toggleClass('menu-open');
 	$('body').toggleClass('noscroll');
   });
